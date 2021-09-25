3edfb007357fe521fe68f5a38d5c9537d04c7bc6
Dayan135 <dayan1357999@gmail.com>
2021-09-25 12:42:47 +0300

    Update index.js
    entire basic requirementes

--- solution/index.js
+++ solution/index.js
@@ -1,16 +1,21 @@
-
+/**
+ * this class orgenize the storage.
+ * vars - tasks - an object that holds all the saved information
+ */
 class myLocalStorage{
     tasks;
     myStorage;
 
-    constructor(){
+    constructor(bool){
         this.tasks = {
             "todo": [],
             "in-progress": [],
             "done": []
         };
         this.myStorage = window.localStorage;
-        // this.myStorage.clear();
+        if(bool){
+            this.myStorage.clear();
+        }
         if(this.myStorage.getItem('tasks')){//if there was local storage earlier
             this.tasks = JSON.parse(this.myStorage.getItem('tasks'));
             this.DOMSync();
@@ -20,29 +25,56 @@ class myLocalStorage{
         }
     }
 
-    DOMSync(){
+    DOMSync(value){
         let elToAppend;
-
+        this.#newWindow()
+        
         for(const key in this.tasks){
             let elId = key;
+
+            //private situation of todo section
             if(elId === "todo") elId = "to-do"
             elId = "section-"+elId;
+
             elToAppend = document.getElementById(elId).children[1]
             for(let child of elToAppend.children){
                 elToAppend.removeChild(child)
             }
-            for(const value of this.tasks[key]){
-                let textEl = document.createElement("p");
-                textEl.textContent = value;
-                let li = document.createElement("li");
-                li.appendChild(textEl);
-                li.addEventListener("dblclick",() => dblClickHandler(li));
-    
-                li.addEventListener("mouseover", () =>document.onkeydown = pressOnHoverHandler)
-                li.addEventListener("mouseout", () => document.onkeydown = null)
-    
-                elToAppend.appendChild(li)
+            let values = this.tasks[key]
+            if(value != undefined){
+                values = [];
+                for(const val of this.tasks[key]){
+                    if(val.includes(value)) {
+                        values.push(val)
+                    }
+                }
             }
+
+            this.#createDOMelements(values, elToAppend);
+        }
+    }
+
+    #newWindow(){
+        let ulElement;
+        for(let key of Object.keys(this.tasks)){
+            if(key ==="todo") key = "to-do"
+            ulElement = document.getElementById("section-"+key).children[1]
+            while(ulElement.firstChild) ulElement.removeChild(ulElement.firstChild)
+        }
+    }
+
+    #createDOMelements(values, elToAppend){
+        for(const value of values){
+            let textEl = document.createElement("p");
+            textEl.textContent = value;
+            let li = document.createElement("li");
+            li.appendChild(textEl);
+            li.addEventListener("dblclick",() => dblClickHandler(li));
+
+            li.addEventListener("mouseover", () =>document.onkeydown = pressOnHoverHandler)
+            li.addEventListener("mouseout", () => document.onkeydown = null)
+
+            elToAppend.appendChild(li)
         }
     }
 
@@ -71,16 +103,34 @@ class myLocalStorage{
     removeVal(key,value){
         let vals = this.getVals(key);
         let index = vals.indexOf(value);
-        // console.log(index)
         if(index < 0) return;
         vals.splice(index,1);
         this.myStorage.setItem('tasks',JSON.stringify(this.tasks))
     }
+
+    findVals(str){
+        let values = [];
+        for(const key in this.tasks){
+
+            for(const value of this.tasks[key]){
+                if(value.toLowerCase().includes(str.toLowerCase())){
+                    values.push(value)
+                }
+            }
+        }
+
+    }
 }
 
 let localStorage = new myLocalStorage();
-localStorage.removeVal('todo', 'a');
-localStorage.print();
+//localStorage.print();
+
+document.getElementById("search").onkeyup = searchKeyDownHandler;
+
+function searchKeyDownHandler(evt){
+    value = document.getElementById("search").value;
+    localStorage.DOMSync(value);
+}
 
 
 function moveElement(elToMove, to){
